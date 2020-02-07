00b7599b764cf807a15b9ee0a21e291d6b421781
Jo Keizer <jolanda@sociossports.nl>
2020-02-07 10:47:42 +0100

    payment options work in progress

--- js/script.js
+++ js/script.js
@@ -118,35 +118,180 @@ checkboxesList.addEventListener('change', (e) => {
 });
 
 
-//checkbox activities
-document.querySelector('.activities').addEventListener('change', (e) => {
-    // for (let i = 0; i < checkboxes.length; i++) {
-    //     const clicked = e.target;
-    //     const clickedTypeDate = clicked.getAttribute('data-day-and-time');
-    //     const clickedTypeCost = clicked.getAttribute('data-cost');
-    //
-    //     if(clicked.checked) {
-    //         totalCost = totalCost + parseInt(clickedTypeCost, 10);
-    //         costEvent.textContent = 'Total $ ' + totalCost;
-    //         console.log('totalCost',totalCost);
-    //         // document.querySelector('#error-checkbox').style.display='block';
-    //     } else if(clicked.checked !== true) {
-    //         console.log("running")
-    //         totalCost = totalCost - parseInt(clickedTypeCost, 10);
-    //         costEvent.textContent = 'Total $ ' + totalCost;
-    //     }
-    //
-    //     //get the date and time on click event
-    //     const checkboxType = checkboxes[i].getAttribute('data-day-and-time');
-    //
-    //     //if the date and time are equal the event is disabled
-    //     if (clickedTypeDate === checkboxType && clicked !== checkboxes[i]) {
-    //         if (clicked.checked) {
-    //             checkboxes[i].disabled = true;
-    //         } else {
-    //             checkboxes[i].disabled = false;
-    //         }
-    //     }
-    // }
+/*
+    Payment Info show one payment on selection
+ */
+
+const payment = document.querySelector('#payment');
+const creditCardElement = document.querySelector('#credit-card');
+const payPalElement = document.querySelector('#paypal');
+const bitcoinElement = document.querySelector('#bitcoin');
+
+//show Select payment method so it doesn’t show up in the drop down
+const selectPayment = document.querySelector('option[value="select method"]');
+const selectCreditCard = document.querySelector('option[value="credit card"]');
+selectPayment.style.display = "none";
+selectCreditCard.style.display = true;
+payPalElement.style.display = "none";
+bitcoinElement.style.display = "none";
+
+payment.addEventListener('change', (e) => {
+    const clicked = e.target.value;
+    if(clicked === 'credit card') {
+        creditCardElement.style.display = '';
+        payPalElement.style.display = "none";
+        bitcoinElement.style.display = "none";
+    } else if (clicked === 'paypal'){
+        creditCardElement.style.display = "none";
+        payPalElement.style.display = "";
+        bitcoinElement.style.display = "none";
+    } else if(clicked === 'bitcoin') {
+        creditCardElement.style.display = "none";
+        payPalElement.style.display = "none";
+        bitcoinElement.style.display =  ""
+    }
 });
 
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#mail');
+const creditCardNumberInput = document.querySelector('#cc-num');
+const zipInput = document.querySelector('#zip');
+const cvvInput = document.querySelector('#cvv');
+const creditCardNumberRegex = /^\d{13,16}$/;
+const zipRegex = /^\d{5}$/;
+const cvvRegex = /^\d{3}$/;
+/*
+    Form Validation with validation expressions
+ */
+
+
+const emailRegex = /^[^@]+@[^@.]+\.[a-z]+$/i;
+
+
+// Can only contain letters a-z in lowercase
+function isValidUsername(username) {
+    return /^[a-z]+$/.test(username);
+}
+
+// check if the eMail is valid
+const isValidEmail = (input) => {
+    return emailRegex.test(input);
+};
+
+
+// check if the credit card section is valid
+
+const isCreditCardValid = (input) => {
+
+    if (selectCreditCard.selected === true) {
+        document.querySelector('#error-creditcard').textContent = "13-16 Numbers only";
+        return creditCardNumberRegex.test(input);
+
+    }
+};
+
+const isZipValid = (input) => {
+
+    if (selectCreditCard.selected === true) {
+        return zipRegex.test(input);
+    }
+};
+
+const isCvvValid = (input) => {
+
+    if (selectCreditCard.selected === true) {
+        return cvvRegex.test(input);
+    }
+};
+
+
+
+// check if any checkbox is selected
+const checkboxSelected = () => {
+    console.log('running')
+    let counter = 0;
+    for (let i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i].checked === true) {
+            counter = +1;
+        }
+    }
+    if (counter === 0) {
+        return false;
+    } else {
+        return true;
+    }
+};
+
+document.querySelector('button[type="submit"]').addEventListener('click', (event) => {
+    if (isValidUsername(nameInput.value) === false) {
+        event.preventDefault();
+        showOrHideErrorMessage(true, nameInput.previousElementSibling);
+    }
+    if (isValidEmail(emailInput.value) === false) {
+        event.preventDefault();
+        showOrHideErrorMessage(true, emailInput.previousElementSibling);
+    }
+    if(checkboxSelected() === false) {
+        console.log("running")
+        event.preventDefault();
+        showOrHideErrorMessage(true, document.querySelector('#error-checkbox'));
+    }
+    if (selectCreditCard.selected === true &&creditCardNumberInput.value === '' || isCreditCardValid(creditCardNumberInput.value) === false) {
+        event.preventDefault();
+        document.querySelector('#error-creditcard').textContent = "Cant't be blank, it should have 13-16 Numbers";
+        showOrHideErrorMessage(true, creditCardNumberInput.previousElementSibling);
+    }
+    if (selectCreditCard.selected === true &&isZipValid(zipInput.value) === false) {
+        event.preventDefault();
+        showOrHideErrorMessage(true, zipInput.previousElementSibling);
+    }
+    if (selectCreditCard.selected === true && isCvvValid(cvvInput.value) === false) {
+        event.preventDefault();
+        showOrHideErrorMessage(true, cvvInput.previousElementSibling);
+    }
+});
+
+// show element when show is true, hide when false
+const showOrHideErrorMessage = (show, element) => {
+    if (show) {
+        element.style.display = '';
+    } else {
+        element.style.display = 'none';
+    }
+};
+
+function errorMessage (message, parent, addDivTo, IdName) {
+    const newDiv = document.createElement('div');
+    newDiv.setAttribute('ID', IdName);
+    newDiv.className = 'error';
+    newDiv.textContent = message;
+    parent.insertBefore(newDiv, addDivTo);
+    newDiv.style.display = 'none';
+
+};
+
+
+errorMessage("What is your name?", document.querySelector('fieldset'), nameInput, 'error-name');
+errorMessage('Oh no? It\'s not a valid Email', document.querySelector('fieldset'), emailInput, 'error-email');
+errorMessage('What would you do for activity?', checkboxesList, document.querySelector('fieldset[class="activities"] label'), 'error-checkbox');
+errorMessage('13-16 Numbers only', document.querySelector('div[class="col-6 col"] '), creditCardNumberInput, 'error-creditcard');
+errorMessage('5 Numbers only', document.querySelector('div[class="col-3 col"] '), zipInput, 'error-zip');
+errorMessage('3 Numbers only', document.querySelector('div[class="col-3 col"] '), cvvInput, 'error-cvv');
+
+//function to create a Event listener and calls the validation function(s)
+function createListener(validator)  {
+    return e => {
+        const text = e.target.value;
+        const valid = validator(text);
+        const showTip = text !== "" && !valid;
+        const message = e.target.previousElementSibling;
+        showOrHideErrorMessage(showTip, message);
+    };
+};
+
+
+nameInput.addEventListener("input", createListener(isValidUsername));
+emailInput.addEventListener("input", createListener(isValidEmail));
+creditCardNumberInput.addEventListener("input", createListener(isCreditCardValid));
+zipInput.addEventListener("input", createListener(isZipValid));
+cvvInput.addEventListener("input", createListener(isCvvValid));
